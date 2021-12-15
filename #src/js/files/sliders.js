3871bdeb9758b18bf7c16aa5e3b1f5a52449b75f
Boris <boris021@mail.ru>
2021-12-15 10:01:07 +0500

    fx

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -27,3 +27,33 @@ var swiper = new Swiper(".mySwiper", {
 		}
 	}
 });
+
+var swiper = new Swiper(".myLpSwiper", {
+	slidesPerView: 3,
+	spaceBetween: 25,
+	loop: true,
+	watchOverflow: true,
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction"
+	},
+	navigation: {
+		nextEl: ".work-swiper-button-next",
+		prevEl: ".work-swiper-button-prev"
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 15
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+			loop: false
+		}
+	}
+});
