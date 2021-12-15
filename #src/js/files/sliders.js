61240ae6a83895aff7e6658bb11050b9613dee61
Boris <boris021@mail.ru>
2021-12-15 14:41:11 +0500

    fx

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -31,6 +31,7 @@ var swiper = new Swiper(".mySwiper", {
 var swiper = new Swiper(".myLpSwiper", {
 	slidesPerView: 3,
 	spaceBetween: 25,
+	// autoHeight: true,
 	loop: true,
 	watchOverflow: true,
 	pagination: {
@@ -52,8 +53,32 @@ var swiper = new Swiper(".myLpSwiper", {
 		},
 		992: {
 			slidesPerView: 3,
-			spaceBetween: 30,
+			spaceBetween: 50,
 			loop: false
+		}
+	}
+});
+
+var swiper = new Swiper(".myPrtSwiper", {
+	slidesPerView: 3,
+	spaceBetween: 25,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination-portfolio",
+		dynamicBullets: true,
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 15
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 50,
 		}
 	}
 });
