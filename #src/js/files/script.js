292241efce61bff39a43c2faab6bf4ff079bfb76
Boris <boris021@mail.ru>
2022-01-17 16:30:39 +0500

    fix table

---
+++ #src/js/files/script.js
@@ -0,0 +1,111 @@
+tippy('.tippy1', {
+	content: "Уникальный дизайн проходит отдельное утверждение. Для выбранного концепта дизайна доступно до 3 итераций правок",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy2', {
+	content: "Создание удобной структуры, написание продающего контента и подбор лицензионных изображений",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy3', {
+	content: "Макет сайта, подстраивающийся под размер дисплея от небольшого смартфона до широкоформатного монитора",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy4', {
+	content: "Метод верстки напрямую влияет на оптимизацию сайта, скорость загрузки и рейтинг поисковых систем",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy5', {
+	content: "Блок сайта - это логическая еденица контента, как правило умещающаяся в один экран",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy6', {
+	content: "Анимация плавного появления блоков при скролле сайта",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy7', {
+	content: "Статическая система управления - позволяет самостоятельно изменять только текст на сайте. Продвинутая - редактировать блоки (преимущества, товары, услуги и т.д.) и изображения",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy8', {
+	content: "Разработка веб-версии логотипа. Для выбранного варианта доступно до 3 итераций правок",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy9', {
+	content: "Консультируйте клиентов на сайте прямо с вашего смартфона в реальном времени",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy10', {
+	content: "Настройка корпоративной почты вида info@вашдомен.kz, а также привязка к почтовому сервису. Также у вас будет возможность самостоятельно создавать почтовые аккаунты с вашим доменом",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy11', {
+	content: "Настройка систем аналитики Google Analitycs и Яндекс Метрика",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy12', {
+	content: "Настройка отслеживания заполнения форм на сайте и отправки данных в систему аналитики для точного расчета эффективности рекламных каналов",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy13', {
+	content: "Все заявки будут сохраняться в базе данных. У вас будет возможность скачать excel файл с сзаявками за выбранный период",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy14', {
+	content: "Настройка отправки заявок в вашу CRM-систему. Интеграция производится только с системами AmoCRM и Bitrix24",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy15', {
+	content: "Тестируйте и получайте больше клиентов. Создание 2 версий сайта с разными вариантами главного экрана для выбора лучшего варианта",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
+
+tippy('.tippy16', {
+	content: "Получайте больше информации и заявок от клиентов с квиз-анкетированием у вас на сайте",
+	arrow: true,
+	placement: 'top',
+	animation: 'fade',
+});
